e861e331c2df15c9c130af219b68fdb0bfc6fda5
Shubh Pandey <shubhpandey2985@gmail.com>
2026-07-16 19:44:30 +0530

    Add files via upload

---
+++ include/utils.h
@@ -0,0 +1,9 @@
+﻿#ifndef UTILS_H
+#define UTILS_H
+#include <stddef.h>
+void printMainMenu(void);
+void readLine(const char *prompt, char *value, size_t size);
+int readInt(const char *prompt);
+float readPositiveAmount(const char *prompt);
+float readNonnegativeAmount(const char *prompt);
+#endif
